dab2aa1f839261bcc63c96fa3d798e248fc4bf83
Allan <mail@allanreid.co>
2022-01-08 02:04:47 -0800

    app finished

--- simple_checklist.py
+++ simple_checklist.py
@@ -1,6 +1,6 @@
 import tkinter
 from tkinter import font
-from tkinter import END
+from tkinter import END, ANCHOR
 
 #Define window
 root = tkinter.Tk()
@@ -19,7 +19,37 @@ root.config(bg=root_color)
 def add_item():
     """Add an individual item to the listbox"""
     my_listbox.insert(END, list_entry.get())
-    list_entry.delete(0, 5)
+    list_entry.delete(0, END)
+
+def remove_item():
+    """Remove the selected (ANCHOR) item from the listbox"""
+    my_listbox.delete(ANCHOR)
+
+def clear_list():
+    """Delete all items from the listbox"""
+    my_listbox.delete(0, END)
+
+def save_list():
+    """Save the list to a simple txt file"""
+    with open('checklist.txt', 'w') as f:
+        #listbox.get() returns tuple....
+        list_tuple = my_listbox.get(0, END)
+        for item in list_tuple:
+            #Take proper precautions to include only one \n for formatting purposes
+            if item.endswith('\n'):
+                f.write(item)
+            else:
+                f.write(item + "\n")
+
+def open_list():
+    """Open the list upon starting the program if there is one"""
+    try:
+        with open('checklist.txt', 'r') as f:
+            for line in f:
+                my_listbox.insert(END, line)
+    except:
+        return
+
 
 #Define Layout
 #Create frames
@@ -45,15 +75,17 @@ my_listbox.grid(row=0, column=0)
 my_scrollbar.grid(row=0, column=1, sticky="NS")
 
 #Button Frame Layout
-list_remove_button = tkinter.Button(button_frame, text='Remove Item', borderwidth=2, font=my_font, bg=button_color)
-list_clear_button = tkinter.Button(button_frame, text='Clear List', borderwidth=2, font=my_font, bg=button_color)
-save_button = tkinter.Button(button_frame, text='Save List', borderwidth=2, font=my_font, bg=button_color)
+list_remove_button = tkinter.Button(button_frame, text='Remove Item', borderwidth=2, font=my_font, bg=button_color, command=remove_item)
+list_clear_button = tkinter.Button(button_frame, text='Clear List', borderwidth=2, font=my_font, bg=button_color, command=clear_list)
+save_button = tkinter.Button(button_frame, text='Save List', borderwidth=2, font=my_font, bg=button_color, command=save_list)
 quit_button = tkinter.Button(button_frame, text='Quit', borderwidth=2, font=my_font, bg=button_color, command=root.destroy)
 list_remove_button.grid(row=0, column=0, padx=2, pady=10)
 list_clear_button.grid(row=0, column=1, padx=2, pady=10, ipadx=10)
 save_button.grid(row=0, column=2, padx=2, pady=10, ipadx=10)
 quit_button.grid(row=0, column=3, padx=2, pady=10, ipadx=25)
 
+#Open previous list if available
+open_list()
 
 #Run the root winow's main loop
 root.mainloop()
